dc74f7d410fb6a40c0b65c7a7deccd781d05132d
Norton ;3 <152797704+Norton42qq@users.noreply.github.com>
2025-12-15 22:04:06 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+cd ~
+git clone https://github.com/Norton42qq/hyprland-rice/
+cd hyprland-rice
+mv hypr/ ~/.config/ && mv waybar/ ~/.config/ && mv wofi/ ~/.config/
+rm -rf ~/hyprland-rice
+clear
+echo "райс успешно установлен. Пожалуйста, навсякий случай перезапустите сессию hyprland"
